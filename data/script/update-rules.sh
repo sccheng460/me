e7b95d076c63249e56ef78c2c35e53de822a296b
scc <75209566+sccheng460@users.noreply.github.com>
2023-12-01 08:44:21 +0800

    更新 update-rules.sh

--- data/script/update-rules.sh
+++ data/script/update-rules.sh
@@ -35,7 +35,7 @@ rules=(
 "https://raw.githubusercontent.com/xinggsf/Adblock-Plus-Rule/master/rule.txt"  #乘风过滤
 "https://anti-ad.net/easylist.txt"  #adni
 "https://easylist-downloads.adblockplus.org/fanboy-annoyance.txt"  #Fanboy’s Annoyances List
-
+"https://cdn.jsdelivr.net/gh/5-whys/adh-rules@release/output_full.txt"  #火柴
 )
  
 allow=(

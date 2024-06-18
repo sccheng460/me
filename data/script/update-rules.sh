d751b9f96f3a3e8fe8f370821fe6d994a1601224
scc <75209566+sccheng460@users.noreply.github.com>
2024-06-18 23:58:38 +0800

    更新 update-rules.sh

--- data/script/update-rules.sh
+++ data/script/update-rules.sh
@@ -37,8 +37,6 @@ rules=(
 "https://raw.githubusercontent.com/qq5460168/me/master/rules.txt"#qq 
 "https://raw.hellogithub.com/hosts"  #github加速
 "https://jsd.onmicrosoft.cn/gh/TG-Twilight/AWAvenue-Adblock-Rule@main/AWAvenue-Adblock-Rule.txt"   #秋风
-
-"https://cdn.jsdelivr.net/gh/5-whys/adh-rules@release/output_full.txt"  #火柴
 )
 
 allow=(
